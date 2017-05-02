683662597dfcd07646af257be90337362f232369
Ulle84 <u.belitz@gmx.de>
2017-05-02 14:06:15 +0200

    + portable PStart

--- Portable/startWork.bat
+++ Portable/startWork.bat
@@ -1,13 +1,13 @@
 rem created on 2017-03-27 by Ulrich Belitz
 @echo off
 
-start ..\ToolBox\bin\SitAndStandTimer.exe
+start ..\..\ToolBox\bin\SitAndStandTimer.exe
 start .\AutoHotkey\AutoHotkeyU64.exe .\AutoHotkey\UserScripts\ScientaOmicron.ahk
 start .\AutoHotkey\AutoHotkeyU64.exe .\AutoHotkey\UserScripts\Shortcuts.ahk
 start .\Ditto\Ditto.exe
 start .\Greenshot\Greenshot.exe
 start .\Notepad++\Notepad++.exe
-start .\PStart\PStart.exe
+start .\PStart\PStart.exe .\PStart\
 start .\Sizer\sizer.exe
 start .\ToDoList\ToDoList.exe D:\ube\WorkDocumentation\ToDoList.tdl
 start .\doublecmd\doublecmd.exe
